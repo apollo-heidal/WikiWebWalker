ba4a9fea304aaddff5cffda58c2ad83b52fbaee2
Apollo Heidal <apolloheidal@gmail.com>
2021-05-05 12:38:53 -0700

    project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WikiWebWalker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
